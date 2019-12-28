15a65d69c549a23d9fc09a0c59cd777641377cd3
npafitis <npafit01@cs.ucy.ac.cy>
2019-12-28 23:28:47 +0200

    Converting paths from map to vector

--- src/clj/post_forms/routes/swagger.clj
+++ src/clj/post_forms/routes/swagger.clj
@@ -4,10 +4,20 @@
             [ring.util.response :refer [response]]
             [ring.util.http-response :as http-response]))
 
+(defn paths-to-vector [paths]
+  (into []
+        (for [path (keys paths)]
+          (hash-map path (paths path)))))
+
+(defn get-post-endpoints [swagger-json]
+  (let [paths (swagger-json "paths")]
+    (paths-to-vector paths)))
+
 (defn analyze-swagger-handler [request]
-  (->
-   (http-response/ok {:foo "bar"})
-   (http-response/header "Content-Type" "text/plain; charset=utf-8")))
+  (let [swagger-json (first (request :body))]
+    (->
+     (http-response/ok (get-post-endpoints swagger-json))
+     (http-response/header "Content-Type" "text/plain; charset=utf-8"))))
 
 (defn swagger-routes []
   ["/swagger"
