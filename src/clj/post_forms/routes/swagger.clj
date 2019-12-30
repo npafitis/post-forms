d43163927b213ff705af603eecb8ae7e32b5a758
nikolas.p <nikolas.p@trgint.com>
2019-12-30 18:21:10 +0200

    Returning form field json for basic types

--- src/clj/post_forms/routes/swagger.clj
+++ src/clj/post_forms/routes/swagger.clj
@@ -1,22 +1,72 @@
 (ns post-forms.routes.swagger
   (:require [post-forms.middleware :as middleware]
+            [post-forms.utils :refer [map-to-vector]]
             [ring.util.request :refer [body-string]]
             [ring.util.response :refer [response]]
-            [ring.util.http-response :as http-response]))
+            [ring.util.http-response :as http-response]
+            [clojure.string :as str]))
 
-(defn paths-to-vector [paths]
-  (into []
-        (for [path (keys paths)]
-          (hash-map path (paths path)))))
+(def definitions (atom nil))
 
-(defn get-post-endpoints [swagger-json]
-  (let [paths (swagger-json "paths")]
-    (paths-to-vector paths)))
+(defn get-endpoints [swagger-json]
+  (map-to-vector (swagger-json "paths")))
+
+(defn advanced-form-field [class]
+  nil)
+
+(defn basic-form-field [parameter]
+  (let [type (parameter "type")
+        name (parameter "name")]
+    ({"boolean" {:checkbox
+                 {:label name}}
+      "string" {"input-text"
+                {:label name}}
+      "integer" {"input-text"
+                 {:label name
+                  :validation-regex  "\\d+"}}}
+     type)))
+
+
+(defn extract-parameter-field [parameter]
+  (prn "Extracting Parameter Form Field")
+  (if (contains? parameter "schema")
+    (if (contains? (parameter "schema") "$ref")
+      (let [classname (->
+                       parameter
+                       (get "schema")
+                       (get "$ref")
+                       (str/split #"/")
+                       (last))
+            class (@definitions classname)]
+        (advanced-form-field class))
+      nil)
+    (basic-form-field parameter)))
+
+(defn extract-method-form [method]
+  (let [method-key (key method)
+        method-val (val method)]
+    (prn "Extract Method Form")
+    {method-key
+     (for [parameter (method-val "parameters")]
+       (extract-parameter-field parameter))}))
+
+(defn analyze-endpoint-methods [path]
+  (let [endpoint-key (first (keys path)) ;; endpoint-key is the string of the endpoint e.g "/foo/bar
+        endpoint (path endpoint-key)]
+    {endpoint-key
+     (for [method endpoint]
+       (extract-method-form method))}))
+
+(defn analyze-endpoints [swagger-json]
+  (let [paths (get-endpoints swagger-json)]
+    (reset! definitions (swagger-json "definitions"))
+    (for [path paths]
+      (analyze-endpoint-methods path))))
 
 (defn analyze-swagger-handler [request]
-  (let [swagger-json (first (request :body))]
+  (let [swagger-json (request :body)]
     (->
-     (http-response/ok (get-post-endpoints swagger-json))
+     (http-response/ok (analyze-endpoints swagger-json))
      (http-response/header "Content-Type" "text/plain; charset=utf-8"))))
 
 (defn swagger-routes []
